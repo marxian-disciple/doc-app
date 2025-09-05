30ff4ffb1d085aaebb2b575e330609c1a70cf39c
Owethu <2459030@students.wits.ac.za>
2025-09-05 12:24:54 +0200

    Configure for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './firebase/firebase.js';
-import { AuthProvider } from './hooks/useAuth.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>
 );
 
